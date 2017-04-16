a0dd0afd6c8c57d949e831f8acb708acc6d68838
Master-Fate <masterfate@users.noreply.github.com>
2017-04-15 20:00:33 -0400

    Add line count tool option to generate only totals
    The -t or --totalonly switches now allow only the line counts to be
    generated, instead of all the dialogue lines as well. This can be useful
    for a more summary-based overview of the unique dialogue counts,
    especially when used in combination with file globbing.

--- opponents/xml_unique_line_counter.py
+++ opponents/xml_unique_line_counter.py
@@ -15,6 +15,8 @@
 #   python xml_unique_line_counter.py -f path\to\character\behaviour.xml -o path\to\save\output\file
 # To have the lines appear by most frequent first (use -s or --sortfreq):
 #   python xml_unique_line_counter.py -f path\to\character\behaviour.xml -s
+# To output only the total number of unique lines (use -t or --totalonly):
+#   python xml_unique_line_counter.py -f path\to\character\behaviour.xml -t
 
 # Parser:
 # pip install html5lib
@@ -30,6 +32,7 @@ from glob import glob
 
 logger = logging.getLogger(os.path.basename(__file__))
 sort_most_frequent = False
+total_count_only = False
 
 def parse(f):
 
@@ -51,17 +54,20 @@ def parse(f):
     d = Counter(l_)
     ctr = 1
     
-    line_iter_items = d.iteritems()
-    if sort_most_frequent: # if the "-s" flag is enabled, order by most frequent lines first
-        line_iter_items = d.most_common()
-    
-    logger.info('***** Dialogue count: {} *****'.format(f))
-    
-    for k, v in line_iter_items:
-        logger.info('{} --> Frequency: {}, Line count: {}'.format(k.encode('utf-8'), v, ctr))
-        ctr += 1
+    if total_count_only: # only display unique line count totals
+        logger.info('{} : {}'.format(f, len(d)))
+    else:
+        line_iter_items = d.iteritems()
+        if sort_most_frequent: # if the "-s" flag is enabled, order by most frequent lines first
+            line_iter_items = d.most_common()
         
-    logger.info('Unique dialogue count: {}\n'.format(len(d)))
+        logger.info('***** Dialogue count: {} *****'.format(f))
+        
+        for k, v in line_iter_items:
+            logger.info('{} --> Frequency: {}, Line count: {}'.format(k.encode('utf-8'), v, ctr))
+            ctr += 1
+            
+        logger.info('Unique dialogue count: {}\n'.format(len(d)))
 
 
 if __name__ == '__main__':
@@ -69,7 +75,7 @@ if __name__ == '__main__':
     output_dir = os.path.dirname(__file__)
 
     argv = sys.argv[1:]
-    opts, args = getopt.getopt(argv, "d:vf:F:o:s", ["download=", "verbose", "file=", "File=", "output=", "sortfreq"])
+    opts, args = getopt.getopt(argv, "d:vf:F:o:st", ["download=", "verbose", "file=", "files=", "output=", "sortfreq", "totalonly"])
     for opt, arg in opts:
         if opt in ("-v", "--verbose"):
             verbose = True
@@ -84,6 +90,8 @@ if __name__ == '__main__':
             output_dir = arg
         elif opt in ("-s", "--sortfreq"):
             sort_most_frequent = True
+        elif opt in ("-t", "--totalonly"):
+            total_count_only = True
             
     log_file = os.path.join(output_dir, "line_count.log")
     file_hndlr = logging.FileHandler(log_file, mode='w')
