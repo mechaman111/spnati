02b7eb2d6358c338373d3973a0aac6f9d84914ed
Magnus Holmgren <magnus@kibibyte.se>
2018-04-01 21:25:32 +0200

    Only look for img attributes on state elements, not condition elements for example.

--- opponents/validate_xml_images.py
+++ opponents/validate_xml_images.py
@@ -21,7 +21,7 @@ def check_referenced_images(filename):
 		if entry.tag == "behaviour":
 			for stage in entry:
 				for case in stage:
-					for state in case:
+					for state in case.iter('state'):
 						image_filename = state.attrib["img"]
 						if image_filename not in image_names:
 							print "Image filename \"%s\" used by %s-%s-%s has not been found" % (image_filename, stage.attrib["id"].strip(), case.attrib["tag"].strip(), state.text.strip())
