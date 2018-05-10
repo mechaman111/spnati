1916a7095f55eb810e61f549f886de661c1342f4
Sebastian Mobo <stmobo@gmail.com>
2018-05-10 15:07:45 -0500

    Add python script to host server offline w/ bottle

---
+++ offline_host.py
@@ -0,0 +1,32 @@
+from bottle import get, post, request, response, redirect, route, run, static_file
+import os
+import os.path as osp
+
+host='localhost'
+port=5000
+
+@route('/')
+def redir_index():
+    return redirect('/index.html')
+
+ext_mimetypes = {
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.html': 'text/html',
+    '.xml': 'application/xml'
+}
+
+@route('/<filename:path>')
+def statics(filename):
+    ext = osp.splitext(filename)[-1]
+
+    try:
+        mimetype = ext_mimetypes[ext]
+    except KeyError:
+        mimetype = 'text/plain'
+
+    return static_file(filename, root=os.getcwd(), mimetype=mimetype)
+
+run(host=host, port=port, reloader=True)
