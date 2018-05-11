983bab716de333bfe5dbc77fee6fc943a08724eb
Sebastian Mobo <stmobo@gmail.com>
2018-05-10 20:04:22 -0500

    Don't preload already cached files

--- js/preloader_worker.js
+++ js/preloader_worker.js
@@ -4,14 +4,28 @@
  */
 
 const CACHE_NAME = 'SPNATI-v1'; // this isn't shared between this context and the ServiceWorker context...
+const CACHE_KEEPALIVE = 600;
 
 onmessage = function (event) {
     console.log("[PL] Preloading "+event.data.length.toString()+" URLs...");
     caches.open(CACHE_NAME).then(
         function (cache) {
             return Promise.all(event.data.map(async function (url) {
-                var resp = await fetch("../"+url, { headers: { 'X-Worker-Initiated': 'true' } });
+                var cached_response = await cache.match(url);
 
+                if(cached_response) {
+                    var resp_time = new Date(cached_response.headers.get('Date'));
+                    var current_cache_age = resp_time.getTime() - Date.now(); // in milliseconds
+
+                    if(current_cache_age < CACHE_KEEPALIVE * 1000) {
+                        /* We already have fresh content cached.
+                         * Don't fetch it again. */
+                        return;
+                    }
+                }
+
+                /* Otherwise, go and fetch it. */
+                var resp = await fetch("../"+url, { headers: { 'X-Worker-Initiated': 'true' } });
                 return cache.put(url, resp);
             }));
         }
