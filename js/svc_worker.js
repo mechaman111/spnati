eb25861df52ad380ae80f243e8945609f6c63163
Sebastian Mobo <stmobo@gmail.com>
2018-05-15 22:46:56 -0500

    Add more detailed documentation for methods in svc_worker.js

--- js/svc_worker.js
+++ js/svc_worker.js
@@ -60,18 +60,42 @@ if ('serviceWorker' in navigator) {
     request_url_caching(cards.concat(static_images));
 }
 
-/* Predicate: can we use SWs at all? */
+/************************************************************
+ * Test if Service Worker features are available.
+ *
+ * If this is true, then request_url_caching()-- if the
+ * SW is unavailable at call time (i.e. if it hasn't been loaded yet)
+ * then the caching request will be queued until the SW is available.
+ *************************************************************/
 function sw_is_available() {
     return ('serviceWorker' in navigator);
 }
 
+
+/************************************************************
+ * Test if the Service Worker is active for this page.
+ *
+ * This probably will not be the case until a bit after the
+ * Service Worker is loaded and active-- if you're preloading
+ * URLs, you might want to use `sw_is_available()` instead,
+ * since we can queue URLs to be preloaded even before the
+ * Service Worker is available.
+ ************************************************************/
 function sw_is_active() {
     /* check to see if we can load SWs in the first place
      * then make sure that the Controller object is not null/undefined/etc.
      */
     return ('serviceWorker' in navigator) && (navigator.serviceWorker.controller != null);
 }
-
+/************************************************************
+ * Sends a message to the service worker.
+ * See the 'message' event listener in service_worker.js
+ * for more details.
+ *
+ * Messages should be objects with 'type' properties at the
+ * very least, and possibly other properties depending on the
+ * message type.
+ ************************************************************/
 function send_msg_to_sw(msg) {
     return new Promise(
         function(resolve, reject) {
@@ -90,6 +114,17 @@ function send_msg_to_sw(msg) {
     );
 }
 
+/************************************************************
+ * Schedule a list of URLs to be preloaded and cached in the background.
+ *
+ * Use this function when you expect to be loading a lot of heavy files
+ * later on; for example, this is called when a character is selected
+ * (to preload said character's gameplay images).
+ *
+ * Preloading is done in the background-- it shouldn't block the main flow
+ * of gameplay (rendering, etc.) but there may be a few pauses / lag-spikes
+ * when beginning to load lots of heavy files.
+ ************************************************************/
 function request_url_caching(urls) {
     if(!sw_is_active()) {
         return new Promise(
@@ -107,7 +142,15 @@ function request_url_caching(urls) {
 
 }
 
-/* Set debug mode true/false in the ServiceWorker */
+
+/************************************************************
+ * Turn debug mode on and off in the Service Worker.
+ * When debug mode is enabled, all non-image content (scripts, dialogue, etc.) will be
+ * fetched immediately instead of using the cache, regardless of cache age.
+ *
+ * Note that the browser may still decide to use its own disk caching system
+ * regardless of the debug mode setting!
+ ************************************************************/
 function set_sw_debug(debug_status) {
     return send_msg_to_sw({ 'type': 'set-debug', 'debug': debug_status}).then(function (ok) {
         if(!ok) {
