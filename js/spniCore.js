73855d37986d899abc1dfe320bdce407ca2ed592
ReformCopyright <magnus@kibibyte.se>
2018-05-24 00:14:17 +0200

    Dynamically resize background to keep scale relative to game area, allowing it to extend a bit outside it.

--- js/spniCore.js
+++ js/spniCore.js
@@ -17,6 +17,9 @@ var HUMAN_PLAYER = 0;
 
 /* Directory Constants */
 var IMG = 'img/';
+
+var backgroundImage;
+
 /*var OPP = 'opponents/';
 #The "OPP" folder abbreviation was used to slightly shorten a few lines in spniSelect that looked for opponents in the opponents folder.
 #Now that opponents can be specified in any folder, this is no longer required.*/
@@ -372,7 +375,20 @@ function autoResizeFont ()
 	/* resize font */
 	var screenWidth = getScreenWidth();
 	document.body.style.fontSize = (14*(screenWidth/1000))+'px';
-	
+
+	if (backgroundImage && backgroundImage.height && backgroundImage.width) {
+		var w = window.innerWidth, h = window.innerHeight;
+		if (h > (3/4) * w) {
+			h = (3/4) * w;
+		} else {
+			w = Math.round((4 * h) / 3);
+		}
+		if (backgroundImage.height > (3/4) * backgroundImage.width) {
+			$("body").css("background-size", "auto " + Math.round(1.12 * h) + "px");
+		} else {
+			$("body").css("background-size", Math.round(1.12 * w) + "px auto");
+		}
+	}
 	/* set up future resizing */
 	window.onresize = autoResizeFont;
 }
