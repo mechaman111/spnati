e5e7ac3dc196ef881559451c1dc202dc0b8a2f33
Sebastian Mobo <stmobo@gmail.com>
2018-06-22 12:42:34 -0400

    Monika: more behaviour line updates

--- opponents/monika/helper-scripts/csv2xml.py
+++ opponents/monika/helper-scripts/csv2xml.py
@@ -18,7 +18,7 @@ import time
 from behaviour_parser import parse_file, parse_meta
 from ordered_xml import OrderedXMLElement
 
-VERSION = '0.13.0-alpha'  # will attempt to follow semver if possible
+VERSION = '0.13.1-alpha'  # will attempt to follow semver if possible
 COMMENT_TIME_FORMAT = 'at %X %Z on %A, %B %d, %Y'  # strftime format
 WARNING_COMMENT = 'This file was machine generated using csv2xml.py {:s} {:s}. Please do not edit it directly without preserving your improvements elsewhere or your changes may be lost the next time this file is generated.'
 
@@ -1033,6 +1033,9 @@ def csv_to_lineset(dict_reader):
                 cond_map[cond_set] = ([], actual_case_tag, row['conditions'], priority)
 
             state_text = preprocess_state_text(row['text'], actual_case_tag, cond_set)
+
+            if len(state_text.strip()) == 0:
+                print("Warning: empty state found (stages {}, case {}, row {})".format(row['stage'], row['case'], line_no))
 
             cond_map[cond_set][0].append(State(state_text, row['image'], marker, silent))
 
