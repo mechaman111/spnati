e94ec60ddf5e020319d428f8312fbe6c2af7742b
Sebastian Mobo <stmobo@gmail.com>
2018-07-21 10:34:32 -0500

    Sayori: updates to fetch_sheets.py

--- opponents/sayori/fetch_sheets.py
+++ opponents/sayori/fetch_sheets.py
@@ -5,29 +5,51 @@ from httplib2 import Http
 from oauth2client import file, client, tools
 import argparse
 import logging
+import functools
 
 parser = argparse.ArgumentParser(parents=[tools.argparser])
+parser.add_argument('--secrets-file', '-s', default='client_id.json', help='Path to the secrets file.')
+parser.add_argument('--store-file', '-t', default='token.json', help='Path to store the client token under.')
+parser.add_argument('--out-dir', '-o', default='.', help='Relative output path to store fetched files under.')
 flags = parser.parse_args()
 
 # Setup the Sheets API
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-store = file.Storage('token.json')
+store = file.Storage(flags.store_file)
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_id.json', SCOPES)
+    flow = client.flow_from_clientsecrets(flags.secrets_file, SCOPES)
     creds = tools.run_flow(flow, store, flags)
 service = build('sheets', 'v4', http=creds.authorize(Http()))
 
 SPREADSHEET_ID = "1kQ--XoQjA6yRqfpDgko7Q-vviu6rs6dE0E0wH-wGt1M"
+
+@functools.lru_cache()
+def get_all_sheet_info():
+    sheet_info = service.spreadsheets().get(spreadsheetId=SPREADSHEET_ID).execute()
+    return sheet_info.get('sheets', [])
+
 def get_data(sheet_name, sheet_range='A1:Z900'):
-    logging.info("Fetching sheet: {:s}...".format(sheet_name))
+    sheet_info = get_all_sheet_info()
+    full_range = None
+    
+    for sheet in sheet_info:
+        title = sheet['properties']['title']
+        n_rows = sheet['properties']['gridProperties']['rowCount']
+        
+        if title == sheet_name:
+            full_range = "'{:s}'!A1:Z{:d}".format(sheet_name, n_rows)
+            logging.info("Grabbing {:d} rows from {:s}...".format(n_rows, sheet_name))
+            break
+    else:
+        raise FileNotFoundError("Could not find sheet {:s} in spreadsheet!".format(sheet_name))
     
     full_range = "'{:s}'!{:s}".format(sheet_name, sheet_range)
     result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
                                                  range=full_range).execute()
     return result.get('values', None)
 
-OUT_DIR = Path('.')
+OUT_DIR = Path(flags.out_dir)
 
 # Maps sheets to filenames
 ALL_SHEETS = {
--- opponents/sayori/generate_behaviour.sh
+++ opponents/sayori/generate_behaviour.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-python.exe ./fetch_sheets.py |& tee convert.log
+python.exe ./fetch_sheets.py \
+    -s ~/Programming/spnati-client-secrets.json \
+    -t ~/Programming/spnati-fetch-token.json \
+    -o ./ |& tee convert.log
 python.exe ../monika/helper-scripts/convert.py ./csvs ./behaviour.xml |& tee -a convert.log
 python.exe ../monika/helper-scripts/check_character.py sayori |& tee -a convert.log
