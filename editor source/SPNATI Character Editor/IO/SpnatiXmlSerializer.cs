04bfec26650d916161758c9ed67e2ff4ded15d98
Magnus Holmgren <maho@millnet.se>
2018-08-26 00:58:51 +0200

    Check that the XmlAnyElement field is not null.

--- editor source/SPNATI Character Editor/IO/SpnatiXmlSerializer.cs
+++ editor source/SPNATI Character Editor/IO/SpnatiXmlSerializer.cs
@@ -227,7 +227,7 @@ namespace SPNATI_Character_Editor.IO
 			foreach (var field in fields)
 			{
 				XmlAnyElementAttribute any = field.GetCustomAttribute<XmlAnyElementAttribute>();
-				if (any != null)
+				if (any != null && field.GetValue(data) != null)
 				{
 					foreach (XmlElement el in field.GetValue(data) as List<XmlElement>)
 					{
