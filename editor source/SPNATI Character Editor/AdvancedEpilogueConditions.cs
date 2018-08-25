3c0d0b4d7592288250b82dfec70d81eccc512830
Magnus Holmgren <maho@millnet.se>
2018-08-25 21:53:03 +0200

    CE source: add support for epilogue gallery image selection.
    Thanks to FarawayVision.

--- editor source/SPNATI Character Editor/AdvancedEpilogueConditions.cs
+++ editor source/SPNATI Character Editor/AdvancedEpilogueConditions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -57,6 +58,7 @@ namespace SPNATI_Character_Editor
 			selAlsoPlayingAnyMarkers.SelectedItems = _epilogue.AlsoPlayingAnyMarkers?.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
 
 			cboAlsoPlaying.Text = _epilogue.AlsoPlaying;
+			valGalleryImage.Text = _epilogue.GalleryImage;
 		}
 
 		private void cmdOK_Click(object sender, EventArgs e)
@@ -69,6 +71,7 @@ namespace SPNATI_Character_Editor
 			_epilogue.AlsoPlayingAllMarkers = selAlsoPlayingAllMarkers.SelectedItems.Length > 0 ? String.Join(" ", selAlsoPlayingAllMarkers.SelectedItems) : null;
 			_epilogue.AlsoPlayingNotMarkers = selAlsoPlayingNotMarkers.SelectedItems.Length > 0 ? String.Join(" ", selAlsoPlayingNotMarkers.SelectedItems) : null;
 			_epilogue.AlsoPlayingAnyMarkers = selAlsoPlayingAnyMarkers.SelectedItems.Length > 0 ? String.Join(" ", selAlsoPlayingAnyMarkers.SelectedItems) : null;
+			_epilogue.GalleryImage = valGalleryImage.Text;
 		}
 
 		private void markerControl_Enter(object sender, EventArgs e)
@@ -90,6 +93,34 @@ namespace SPNATI_Character_Editor
 		private void valMaxPlayerStartingLayers_ValueChanged(object sender, EventArgs e)
 		{
 			valPlayerStartingLayers.Maximum = valMaxPlayerStartingLayers.Value;
+		}
+
+		private void GalleryImageSelectBtn_Click(object sender, EventArgs e)
+		{
+			string dir = Config.GetRootDirectory(_character);
+			galleryImageFileDialog.InitialDirectory = dir;
+			DialogResult result = DialogResult.OK;
+			bool invalid;
+			do
+			{
+				invalid = false;
+				result = galleryImageFileDialog.ShowDialog();
+				if (result == DialogResult.OK)
+				{
+					if (Path.GetDirectoryName(galleryImageFileDialog.FileName) != dir)
+					{
+						MessageBox.Show("Images need to come from the character's folder.");
+						invalid = true;
+					}
+				}
+			}
+			while (invalid);
+
+			if (result == DialogResult.OK)
+			{
+				string file = Path.GetFileName(galleryImageFileDialog.FileName);
+				valGalleryImage.Text = file;
+			}
 		}
 	}
 }
