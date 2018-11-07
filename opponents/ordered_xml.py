582913e9b6e49d2f18b340e901df575b9de2a720
ReformCopyright <magnus@kibibyte.se>
2018-11-07 21:39:02 +0100

    Properly escape special characters when serializing XML, support comments, and add a blank line between elements with many children on the top levels in order to roughly match the current output of make_xml.py without having to implement the manual prettification stuff.

--- opponents/ordered_xml.py
+++ opponents/ordered_xml.py
@@ -1,8 +1,29 @@
 from __future__ import unicode_literals
-    
+import re
 from collections import OrderedDict
 
 INDENT = '    '
+
+ATTR_ESCAPES = {
+    '<': 'lt',
+    '>': 'gt',
+    '&': 'amp',
+    '"': 'quot',
+    "\n": '#10'
+}
+ATTR_ESCAPE_RE = re.compile('|'.join(ATTR_ESCAPES.keys()))
+
+TEXT_ESCAPES = {
+    '<': 'lt',
+    '>': 'gt',
+    '&': 'amp'
+}
+TEXT_ESCAPE_RE = re.compile('|'.join(TEXT_ESCAPES.keys()))
+
+ENTITY_RE = re.compile('&(?:(\w+)|#(?:(\d+)|x([\da-f]+)));')
+
+def XMLEscape(string, table, regex):
+    return regex.sub(lambda m: '&' + table[m.group(0)] + ';', string)
 
 class OrderedXMLElement(object):
     """Represents an XML element with an ordered set of attributes and either child elements or raw text within."""
@@ -41,30 +62,40 @@ class OrderedXMLElement(object):
         for child in self.children:
             if child.type == find_tag:
                 yield child
-    
-    def __serialize(self):
-        attr_string = " ".join(["{:s}=\"{:s}\"".format(k, v) for k,v in self.attributes.items()])
+
+    def __serialize(self, level=0):
+        attr_string = " ".join(["{:s}=\"{:s}\"".format(k, XMLEscape(v, ATTR_ESCAPES, ATTR_ESCAPE_RE)) for k,v in self.attributes.items()])
         
         if len(attr_string) > 0:
             attr_string = ' '+attr_string
-        
-        if len(self.children) == 0 and self.text is None:
+
+        if self.type is None: # Comment
+            return ["<!--{:s}-->".format(self.text)]
+        elif len(self.children) == 0 and (self.text is None or self.text == ""):
             # <[type] [attributes...] />
             return ["<{:s}{:s} />".format(self.type, attr_string)]
         elif len(self.children) == 0 and self.text is not None:
             # <[type] [attributes...]>[text]</[type]>
-            return ["<{:s}{:s}>{:s}</{:s}>".format(self.type, attr_string, self.text, self.type)]
+            escaped_text = re.sub
+            return ["<{:s}{:s}>{:s}</{:s}>".format(self.type, attr_string, XMLEscape(self.text, TEXT_ESCAPES, TEXT_ESCAPE_RE), self.type)]
         elif len(self.children) > 0 and self.text is None:
             l = ["<{:s}{:s}>".format(self.type, attr_string)]
-            
+            first = True
             for child in self.children:
-                for line in child.__serialize():
-                    l.append(INDENT+line)
-            
+                if not first and (len(child.children) > 9 or (level == 0 and len(child.children) > 0)):
+                    l.append('')
+                for line in child.__serialize(level + 1):
+                    l.append((INDENT+line).rstrip())
+                first = False
+
             l.append("</{:s}>".format(self.type))
             return l
         else:
             raise NotImplementedError("OrderedXMLElements do not support having both child elements and text.")
             
     def serialize(self):
-        return '\n'.join(self.__serialize())
+        return '\n'.join(["<?xml version='1.0' encoding='UTF-8'?>"] + self.__serialize(0)) + '\n'
+
+# This is similar to how ElementTree does it, except we just set the tag to None to signify a comment.
+def Comment(text):
+    return OrderedXMLElement(None, text)
