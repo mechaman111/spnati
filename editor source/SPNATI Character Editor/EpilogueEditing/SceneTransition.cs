a1d935074fc851377ea0950a22a85103a6ba1674
spnati_edit <grandforkian@hotmail.com>
2019-01-14 20:34:21 -0600

    updated transition effects

--- editor source/SPNATI Character Editor/EpilogueEditing/SceneTransition.cs
+++ editor source/SPNATI Character Editor/EpilogueEditing/SceneTransition.cs
@@ -58,6 +58,36 @@ namespace SPNATI_Character_Editor.EpilogueEditing
 				case "wipe-down":
 					Effect = WipeDown;
 					break;
+				case "slide-right":
+					Effect = SlideRight;
+					break;
+				case "slide-left":
+					Effect = SlideLeft;
+					break;
+				case "slide-up":
+					Effect = SlideUp;
+					break;
+				case "slide-down":
+					Effect = SlideDown;
+					break;
+				case "barn-open-horizontal":
+					Effect = OpenBarnDoorHorizontal;
+					break;
+				case "barn-close-horizontal":
+					Effect = CloseBarnDoorHorizontal;
+					break;
+				case "barn-open-vertical":
+					Effect = OpenBarnDoorVertical;
+					break;
+				case "barn-close-vertical":
+					Effect = CloseBarnDoorVertical;
+					break;
+				case "fly-through":
+					Effect = FlyThrough;
+					break;
+				case "spin":
+					Effect = Spin;
+					break;
 				default:
 					Effect = Cut;
 					break;
@@ -86,19 +116,36 @@ namespace SPNATI_Character_Editor.EpilogueEditing
 					{
 						using (Pen pen = new Pen(Color.Black, 10))
 						{
-							g.FillRectangle(View1.Color, View1.X, View1.Y, width, height);
-							brush.Color = pen.Color = Color.FromArgb(View1.Color.Color.A, brush.Color);
-							g.DrawString(View1.Label, font, brush, new RectangleF(View1.X, View1.Y, width, height), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
-							g.DrawRectangle(pen, View1.X, View1.Y, width, height);
-							g.FillRectangle(View2.Color, View2.X, View2.Y, width, height);
-							brush.Color = pen.Color = Color.FromArgb(View2.Color.Color.A, brush.Color);
-							g.DrawString(View2.Label, font, brush, new RectangleF(View2.X, View2.Y, width, height), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
-							g.DrawRectangle(pen, View2.X, View2.Y, width, height);
+							if (View1.OnTop)
+							{
+								DrawView(g, View2, width, height, font, brush, pen);
+								DrawView(g, View1, width, height, font, brush, pen);
+							}
+							else
+							{
+								DrawView(g, View1, width, height, font, brush, pen);
+								DrawView(g, View2, width, height, font, brush, pen);
+							}
+
 							g.FillRectangle(fade, 0, 0, width, height);
 						}
 					}
 				}
 			}
+		}
+
+		private void DrawView(Graphics g, SceneViewport view, int width, int height, Font font, SolidBrush brush, Pen pen)
+		{
+			g.ResetTransform();
+			g.TranslateTransform(width / 2.0f, height / 2.0f);
+			g.ScaleTransform(view.Scale, view.Scale);
+			g.RotateTransform(view.Rotation);
+			g.TranslateTransform(-width / 2.0f, -height / 2.0f);
+			g.Clip = new Region(new RectangleF(view.CropLeft, view.CropTop, width - view.CropRight - view.CropLeft, height - view.CropBottom - view.CropTop));
+			g.FillRectangle(view.Color, view.X, view.Y, width, height);
+			brush.Color = pen.Color = Color.FromArgb(view.Color.Color.A, brush.Color);
+			g.DrawString(view.Label, font, brush, new RectangleF(view.X, view.Y, width, height), new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center });
+			g.DrawRectangle(pen, view.X, view.Y, width, height);
 		}
 
 		public void Dissolve(float t)
@@ -121,28 +168,101 @@ namespace SPNATI_Character_Editor.EpilogueEditing
 			View2.SetAlpha(t < 0.5f ? 0 : 255);
 		}
 
-		public void WipeRight(float t)
+		public void SlideRight(float t)
 		{
 			int left = (int)Math.Ceiling(View2.Width * (1 - t));
 			View2.X = -left;
 		}
 
-		public void WipeLeft(float t)
+		public void SlideLeft(float t)
 		{
 			int left = (int)Math.Ceiling(View2.Width * (1 - t));
 			View2.X = left;
 		}
 
-		public void WipeUp(float t)
+		public void SlideUp(float t)
 		{
 			int top = (int)Math.Ceiling(View2.Height * (1 - t));
 			View2.Y = top;
 		}
 
-		public void WipeDown(float t)
+		public void SlideDown(float t)
 		{
 			int top = (int)Math.Ceiling(View2.Height * (1 - t));
 			View2.Y = -top;
+		}
+
+		public void WipeRight(float t)
+		{
+			View2.CropRight = (int)((1 - t) * View2.Width);
+		}
+
+		public void WipeLeft(float t)
+		{
+			View2.CropLeft = (int)((1 - t) * View2.Width);
+		}
+
+		public void WipeUp(float t)
+		{
+			View2.CropTop = (int)((1 - t) * View2.Height);
+		}
+
+		public void WipeDown(float t)
+		{
+			View2.CropBottom = (int)((1 - t) * View2.Height);
+		}
+
+		public void OpenBarnDoorHorizontal(float t)
+		{
+			View2.CropLeft = View2.CropRight =(int)(View2.Width / 2  * (1 - t));
+		}
+
+		public void CloseBarnDoorHorizontal(float t)
+		{
+			View1.CropLeft = View1.CropRight = (int)(View1.Width / 2 * t);
+			View1.OnTop = true;
+		}
+
+		public void OpenBarnDoorVertical(float t)
+		{
+			View2.CropTop = View2.CropBottom = (int)(View2.Height / 2 * (1 - t));
+		}
+
+		public void CloseBarnDoorVertical(float t)
+		{
+			View1.CropTop = View1.CropBottom = (int)(View1.Height / 2 * t);
+			View1.OnTop = true;
+		}
+
+		public void FlyThrough(float t)
+		{
+			float zoom = MathUtil.Lerp(1.0f, 2.0f, t);
+			View1.Scale = zoom;
+			View1.SetAlpha((int)((1 - t) * 255));
+
+			zoom = MathUtil.Lerp(0.5f, 1, t);
+			View2.Scale = zoom;
+			View2.SetAlpha((int)(t * 255));
+		}
+
+		public void Spin(float t)
+		{
+			if (t < 0.5f)
+			{
+				t *= 2;
+				View1.SetAlpha(255);
+				View2.SetAlpha(0);
+				View1.Rotation = t * 1080;
+				View1.Scale = 5 * t + 1;
+			}
+			else
+			{
+				t = (1 - (t - 0.5f) * 2);
+				View1.SetAlpha(0);
+				View2.SetAlpha(255);
+				View2.Rotation = t * 1080;
+				View2.Scale = 5 * t + 1;
+			}
 		}
 	}
 
@@ -155,6 +275,13 @@ namespace SPNATI_Character_Editor.EpilogueEditing
 		public int Y;
 		public int Width;
 		public int Height;
+		public int CropLeft;
+		public int CropRight;
+		public int CropTop;
+		public int CropBottom;
+		public bool OnTop;
+		public float Rotation;
+		public float Scale = 1;
 
 		public SceneViewport(string label, Color color, int width, int height)
 		{
