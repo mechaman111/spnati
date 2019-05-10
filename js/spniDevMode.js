162bff592b3ae19fa7932d44818247ae56d0e92d
FarawayVision <stmobo@gmail.com>
2019-05-10 08:44:11 -0500

    Dev Mode: change edit log format
    Instead of trying to figure out response cases on its own, saveEdits()
    now outputs information geared towards ID-ing the specific case to respond to,
    to leverage the CE's preexisting functionality for generating response cases.

--- js/spniDevMode.js
+++ js/spniDevMode.js
@@ -67,58 +67,11 @@ DevModeDialogueBox.prototype.update = function (player) {
     if (!player) return;
     
     this.currentState = player.chosenState;
-    this.updateResponseData();
     
     if (devModeActive && devModeTarget === player.slot) {
         this.container.attr('data-dev-target', 'true');
     } else {
         this.container.attr('data-dev-target', null);
-    }
-}
-
-DevModeDialogueBox.prototype.updateResponseData = function () {
-    this.genericResponseTag = this.player.lastUpdateTags[0];
-    this.targetedResponseTag = null;
-    this.phaseIsTargeted = false;
-    
-    var responsePhase = gamePhase;
-    if (inRollback()) {
-        responsePhase = rolledBackGamePhase;
-    }
-    
-    switch (responsePhase) {
-    case eGamePhase.AITURN:
-    case eGamePhase.EXCHANGE:
-    case eGamePhase.REVEAL:
-        // Last played phase was an AI turn or Exchange phase:
-        // Reactions here are either hand quality lines, forfeit lines, or swap lines.
-        // Targeted response tags are generally the same 
-        this.targetedResponseTag = this.genericResponseTag;
-        break;
-    case eGamePhase.PRESTRIP:
-    case eGamePhase.STRIP:
-    case eGamePhase.FORFEIT:
-        // Last played phase was a reveal phase.
-        // Use opponent_lost for targeted responses.
-        this.targetedResponseTag = 'opponent_lost';
-        this.phaseIsTargeted = true;
-        break;
-    case eGamePhase.DEAL:
-        if (currentRound < 0) {
-            // This is the start of a new game.
-            this.targetedResponseTag = this.genericResponseTag;
-        }  else {
-            // Last played phase was a strip phase.
-            this.targetedResponseTag = 'opponent_stripped';
-            this.phaseIsTargeted = true;
-        }
-        break;
-    case eGamePhase.END_FORFEIT:
-    case eGamePhase.GAME_OVER:
-    case eGamePhase.END_LOOP:
-        this.targetedResponseTag = this.genericResponseTag;
-        this.phaseIsTargeted = true;
-        break;
     }
 }
 
@@ -160,39 +113,74 @@ DevModeDialogueBox.prototype.saveEdits = function () {
         // Log an edited state.
         var stateID = this.player.chosenState.id;
         
-        this.player.editLog.push({'type': 'edit', 'stateID': stateID, 'state': {'text': this.currentStateDialogue, 'pose': this.currentStatePose}});
+        this.player.editLog.push({
+            'type': 'edit',
+            'stage': this.player.stage,
+            'tag': this.currentState.parentCase.tag,
+            'oldState': {'text': this.currentState.rawDialogue, 'pose': this.currentState.image},
+            'state': {'text': this.currentStateDialogue, 'pose': this.currentStatePose}
+        });
         
         this.player.xml.find('state[dev-id="'+stateID+'"]').each(function (idx, elem) {
             $(elem).html(this.currentStateDialogue);
         }.bind(this));
     } else {
-        var props = {'type': 'new', 'stage': this.player.stage, 'state': {'text': this.currentStateDialogue, 'pose': this.currentStatePose}};
+        var editEntry = {
+            'type': 'new',
+            'intent': 'generic',
+            'phaseTarget': null,
+            'responseTarget': null,
+            'stage': this.player.stage,
+            'state': {'text': this.currentStateDialogue, 'image': this.currentStatePose},
+            'suggestedTag': this.player.lastUpdateTags[0],
+        };
         
-        if (this.respondingToPlayer) {
-            var target = this.player.currentTarget;
-            props.tag = this.targetedResponseTag;
+        function playerStateInfo (player) {
+            var curState = player.chosenState;
             
-            if (this.phaseIsTargeted) {
-                props.target = this.respondingToPlayer.id;
-                if (target !== players[HUMAN_PLAYER]) {
-                    props.targetStage = target.stage;
-                    props.currentTargetLine = target.chosenState ? target.chosenState.rawDialogue : '';
+            var info = {
+                'id': player.id,
+                'stage': player.stage,
+            }
+            
+            if (curState) {
+                var curCase = curState.parentCase;
+                    
+                info.caseTag = curCase.tag;
+                info.state = {'text': curState.rawDialogue, 'image': curState.image};
+                
+                if (curState.marker) {
+                    info.marker = {
+                        'name': curState.marker.name,
+                        'perTarget': curState.marker.perTarget,
+                    };
+                    
+                    var markerName = curState.marker.name;
+                    if (target.chosenState.marker.perTarget) {
+                        markerName = getTargetMarker(curState.marker.name, player.currentTarget);
+                    }
+                    
+                    info.marker.value = player.markers[markerName];
                 }
             }
             
-            if (this.respondingToPlayer !== target) {
-                props.alsoPlaying = this.respondingToPlayer.id;
-                props.alsoPlayingStage = this.respondingToPlayer.stage;
-                props.alsoPlayingCurrentLine = this.respondingToPlayer.chosenState ? this.respondingToPlayer.chosenState.rawDialogue : '';
-            }
-        } else {
-            props.tag = this.genericResponseTag;
+            return info;
         }
         
-        this.player.editLog.push(props);
+        if (this.respondingToPlayer) {
+            editEntry.intent = 'response';
+            editEntry.responseTarget = playerStateInfo(this.respondingToPlayer);
+            
+            if (this.player.currentTarget) {
+                editEntry.phaseTarget = playerStateInfo(this.player.currentTarget);
+            }
+        }
+        
+        this.player.editLog.push(editEntry);
     }
     
-    this.player.chosenState.rawDialogue = this.currentStateDialogue;    
+    this.player.chosenState.rawDialogue = this.currentStateDialogue;
+    this.player.chosenState.image = this.currentStatePose;
     this.cancelEditMode();
 }
 
@@ -212,13 +200,13 @@ DevModeDialogueBox.prototype.newResponseState = function (respondTo) {
     this.currentStateDialogue = "";
     this.respondingToPlayer = respondTo;
     this.editingState = false;
-    this.updateResponseData();
     
     if (!respondTo) {
-        this.statusLine.text("Generic: "+this.genericResponseTag);
+        this.statusLine.text("New Generic");
+        // this.statusLine.text("Generic: "+this.genericResponseTag);
     } else {
-        //this.statusLine.text("Responding to "+respondTo.label);
-        this.statusLine.text("Targeted: "+this.targetedResponseTag);
+        this.statusLine.text("Response: "+this.respondingToPlayer.label);
+        // this.statusLine.text("Targeted: "+this.targetedResponseTag);
     }
     
     this.startEditMode();
