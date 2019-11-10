b1445ed715ab54707958edda0ac8b8acfb74ba58
FarawayVision <stmobo@gmail.com>
2019-11-09 22:27:15 -0500

    Monika: Unescaping adjustments of some kind?

--- opponents/monika/helper-scripts/unescape_dialogue.py
+++ opponents/monika/helper-scripts/unescape_dialogue.py
@@ -1,39 +1,39 @@
-from csv2xml import behaviour_parser as bp
-from csv2xml.ordered_xml import OrderedXMLElement
-from pathlib import Path
-import sys
-import shutil
 import re
+import shutil
 
-TEXT_UNESCAPES = {
-    '&amp;': '&',
-    '&lt;': '<',
-    '&gt;': '>'
+STATE_MATCH_RE = r"(<state[^>]*>)(.+)(?=</state>)"
+
+ESCAPED_CHARACTERS = {"&amp;": "&", "&lt;": "<", "&gt;": ">"}
+TEXT_UNESCAPE_RE = re.compile("|".join(map(re.escape, ESCAPED_CHARACTERS.keys())))
+
+
+RE_ESCAPE = {
+    "<i>": "&lt;i&gt;",
+    "<br>": "&lt;br&gt;",
+    "<hr>": "&lt;hr&gt;",
+    "</i>": "&lt;/i&gt;",
+    "</br>": "&lt;/br&gt;",
+    "</hr>": "&lt;/hr&gt;",
 }
-TEXT_UNESCAPE_RE = re.compile('|'.join(map(re.escape, TEXT_UNESCAPES.keys())))
 
-def main():
-    shutil.copyfile('./behaviour.xml', './behaviour.xml.unescape.bak')
-    root = bp.parse_file('./behaviour.xml')
 
-    for stage_elem in root.find('behaviour').iter('stage'):
-        for case_elem in stage_elem.iter('case'):
-            for state_elem in case_elem.iter('state'):
-                state_elem.text = TEXT_UNESCAPE_RE.sub(
-                    lambda m: TEXT_UNESCAPES[m.group(0)],
-                    state_elem.text
-                )
-    
-    for trigger_elem in root.find('behaviour').iter('trigger'):
-        for case_elem in trigger_elem.iter('case'):
-            for state_elem in case_elem.iter('state'):
-                state_elem.text = TEXT_UNESCAPE_RE.sub(
-                    lambda m: TEXT_UNESCAPES[m.group(0)],
-                    state_elem.text
-                )
-    
-    with open('behaviour.xml', 'w', encoding='utf-8') as f:
-        f.write(root.serialize(False, False))
+def escape_xml(match):
+    print(match[0])
 
-if __name__ == '__main__':
-    main()
+    t = TEXT_UNESCAPE_RE.sub(lambda m: ESCAPED_CHARACTERS[m.group(0)], match[2])
+
+    for sub_from, sub_to in RE_ESCAPE.items():
+        t = t.replace(sub_from, sub_to)
+
+    print(t)
+
+    return match[1] + t
+
+
+shutil.copyfile("./behaviour.xml", "./behaviour.xml.unescape.bak")
+
+with open("./behaviour.xml", "r") as f:
+    escaped = re.sub(STATE_MATCH_RE, escape_xml, f.read())
+
+with open("./behaviour.xml", "w") as f:
+    f.write(escaped)
