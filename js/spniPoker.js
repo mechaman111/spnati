76307b5850a887ab417284a89adf10cd41c5a4ea
ReformCopyright <magnus@kibibyte.se>
2020-01-29 22:18:37 +0100

    Detect common cheating attempt to avoid being spammed with automatic error reports.
    Should prevent Sentry issues SPNATI-1D, SPNATI-1E4.

--- js/spniPoker.js
+++ js/spniPoker.js
@@ -204,16 +204,8 @@ function clearCard (player, i) {
  ************************************************************/
 function displayCard (player, i, visible) {
     if (players[player].hand.cards[i]) {
+        detectCheat();
         if (visible) {
-            if (typeof players[player].hand.cards[i].altText !== "function" && SENTRY_INITIALIZED) {
-                Sentry.setExtra("player", player);
-                Sentry.setExtra("i", i);
-                Sentry.setExtra("toString_type", typeof players[player].hand.cards[i].toString);
-                Sentry.setExtra("card_type", typeof players[player].hand.cards[i]);
-                Sentry.setExtra("card", players[player].hand.cards[i]);
-                Sentry.setExtra("altText_prototype_type", typeof Card.prototype.altText);
-                Sentry.setExtra("num_bad_cards", players.reduce(function(a, p) { return a.concat(p.hand.cards); }, []).countTrue(c => typeof c != 'object'));
-            }
             $cardCells[player][i].attr({ src: IMG + players[player].hand.cards[i].toString() + ".jpg",
                                          alt: players[player].hand.cards[i].altText() });
         } else {
