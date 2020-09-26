19577ab75770467bc9a23704620d7e88b9fa47f7
ReformCopyright <magnus@kibibyte.se>
2020-09-26 14:05:29 +0200

    Make the expandDialogue() regexp a bit stricter to avoid tildes inside a word followed by a period being parsed as starting a variable expansion.
    The first character after the period after the first word (variable) must now be a word character. In other words, although collectible IDs regrettably can contain spaces and special characters, they cannot begin with one.
    
    Actually, you can get around this by using `~self.collectible.*~` instead of `~collectible.*~`, which is what everyone is already doing, so we could actually require only word characters after the first period, and then after a possible second period you can put whatever you want.

--- js/spniBehaviour.js
+++ js/spniBehaviour.js
@@ -1014,7 +1014,7 @@ function expandDialogue (dialogue, self, target, bindings) {
     // variable or
     // variable.attribute or
     // variable.function(arguments)
-    return dialogue.replace(/~(\w+)(?:\.(.+?))?(?:\(([^)]*)\))?~/g, substitute);
+    return dialogue.replace(/~(\w+)(?:\.(\w.*?))?(?:\(([^)]*)\))?~/g, substitute);
 }
 
 function escapeRegExp(string) {
