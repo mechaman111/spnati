8330485fef8b72cc80d2ffa7b6882e7f50727cc2
FarawayVision <stmobo@gmail.com>
2022-08-02 22:37:47 -0500

    Mari: add compress_image_changes.sh

---
+++ opponents/mari/compress_image_changes.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+# cd to repo root first
+cd $(git rev-parse --show-toplevel)
+git ls-files -m -o --exclude-standard -- opponents/mari opponents/reskins/mari_* | grep png | xargs -t ./tools/pngquant -f --ext .png --skip-if-larger --
